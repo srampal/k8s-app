13aa3b7a7a53c4170fd687a7f1f4b86e0a077bc5
Sanjeev Rampal <srampal@cisco.com>
2017-11-09 06:49:59 +0000

    app delete scripts

---
+++ delete-all-apps.sh
@@ -0,0 +1,4 @@
+./gb-app-delete.sh
+sudo kubectl delete -f redis-sc.yaml
+sudo kubectl delete namespace cafe-app
+
---
+++ gb-app-delete.sh
@@ -0,0 +1,4 @@
+sudo kubectl delete -f gb-ingress.yaml
+sudo kubectl delete -f demo-guestbook.yaml
+sudo kubectl delete -f redis-slave-claim.yaml
+sudo kubectl delete -f redis-master-claim.yaml
